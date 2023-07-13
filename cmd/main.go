38f061f0e97fc881dc1d7781c7d9f0b52f78bb7f
DmitryK <kruglov081289@gmail.com>
2023-07-13 20:41:03 +0300

    feat: add migrations

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	handler2 "rest-api-postgres/internal/handler"
+	"rest-api-postgres/internal/repository"
+	"rest-api-postgres/internal/service"
 	"rest-api-postgres/pkg"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error config initialization: %v", err)
+	}
+	repos := repository.New()
+	services := service.New(repos)
+	handler := handler2.New(services)
+
 	srv := new(pkg.Server)
-	handler := new(handler2.Handler)
-	// TODO pass config
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server: %v", err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"rest-api-postgres/internal/service"
+)
 
 type Handler struct {
+	services *service.Service
+}
+
+func New(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
